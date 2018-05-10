bb198c9dd78c7daa7ea80d28ab8015298d794be4
Douwe Homans <douwe@avocado.nl>
2018-05-10 10:33:31 +0200

    typo
    Signed-off-by: Douwe Homans <douwe@avocado.nl>

--- addon/services/firestore.js
+++ addon/services/firestore.js
@@ -2,6 +2,6 @@ import Service from "@ember/service";
 import { inject as service } from "@ember/service";
 
 export default Service.extend({
-  firebase: service()
-  instance: this.get('firebase').firestore()
+  firebase: service(),
+  instance: this.get("firebase").firestore()
 });
